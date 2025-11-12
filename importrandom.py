602e458a13aa953e9d9a1bf17d617042f2c5f221
sebastiancanas-dot <sebastian.canas@alu.unach.cl>
2025-11-12 00:13:30 -0300

    iniciar pygame

--- importrandom.py
+++ importrandom.py
@@ -1,1 +1,16 @@
+import pygame
+import random
+import math
+import os
 
+# -----------------------------------
+# CONFIGURACIÓN INICIAL
+# -----------------------------------
+ANCHO, ALTO = 800, 600
+FPS = 20
+GROSOR_ORILLA = 20
+
+pygame.init()
+ventana = pygame.display.set_mode((ANCHO, ALTO))
+pygame.display.set_caption("Simulador de Ecosistema (Con Casa)")
+reloj = pygame.time.Clock()
