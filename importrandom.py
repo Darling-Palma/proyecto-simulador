9979eceac45a5c43fc061b13808769efe3fabdf0
sebastiancanas-dot <sebastian.canas@alu.unach.cl>
2025-11-12 00:20:55 -0300

    configuracion rutas de imagenes

--- importrandom.py
+++ importrandom.py
@@ -14,3 +14,10 @@ pygame.init()
 ventana = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Simulador de Ecosistema (Con Casa)")
 reloj = pygame.time.Clock()
+# --- Definición de rutas de imágenes ---
+try:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    BASE_DIR = os.getcwd()
+IMAGES_DIR = os.path.join(BASE_DIR, "imagenes")
+# ----------------------------------------------
